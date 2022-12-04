e338950c7d5cb321b131f7e2e0a91c30a81745da
Betsy Martinez Orozco <betsymartinezorozco@gmail.com>
2022-12-04 03:01:49 -0600

    Creando modal para ver y editar la informacion de los usuarios

--- src/app/administration/users/user-details/user-details.component.ts
+++ src/app/administration/users/user-details/user-details.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {User} from '../../../models/user';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-details',
@@ -8,11 +9,28 @@ import {User} from '../../../models/user';
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
-  userToShow: User;
   constructor(private formBuilder: FormBuilder,
-              /*private activeModal: NgbActiveModal*/) { }
+              public dialogRef: MatDialogRef<UserDetailsComponent>,
+              @Inject(MAT_DIALOG_DATA) private data: any) { }
+  userToShow: User;
+  form: FormGroup;
 
   ngOnInit() {
+    this.userToShow = this.data.userToShow;
+    this.initForm();
   }
 
+  initForm() {
+    this.form = new FormGroup({
+      name: new FormControl(this.userToShow.name),
+      username: new FormControl(this.userToShow.username),
+      company: new FormControl(this.userToShow.company.name),
+      email: new FormControl(this.userToShow.email),
+      phone: new FormControl(this.userToShow.phone),
+    });
+  }
+
+  closeDialog() {
+    this.dialogRef.close(this.userToShow);
+  }
 }
