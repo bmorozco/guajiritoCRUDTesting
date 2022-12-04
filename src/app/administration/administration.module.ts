5426acf39e05a6afc9b4004c5acc93abe3e3adc9
Betsy Martinez Orozco <betsymartinezorozco@gmail.com>
2022-12-04 03:03:28 -0600

    Añadiendo modulos para ver datos de material con sus respectivos estilos

--- src/app/administration/administration.module.ts
+++ src/app/administration/administration.module.ts
@@ -1,19 +1,30 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {UsersComponent} from './users/users.component';
 import {AdministrationRoutingModule} from './administration-routing.module';
 import { AdministrationComponent } from './administration.component';
 import { UserDetailsComponent } from './users/user-details/user-details.component';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [UsersComponent, AdministrationComponent, UserDetailsComponent],
   imports: [
-    CommonModule,
     AdministrationRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatTableModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDialogModule,
+    MatButtonModule
   ],
-  exports: [UserDetailsComponent],
+  exports: [UserDetailsComponent, UsersComponent],
   entryComponents: [UserDetailsComponent]
 })
 
