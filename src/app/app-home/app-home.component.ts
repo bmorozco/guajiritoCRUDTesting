9ce30f84c7ddb2b0bc7922275648e230ad9c1058
Betsy Martinez Orozco <betsymartinezorozco@gmail.com>
2022-12-05 02:54:09 -0600

    Añadiendo sidebar/navbar

--- src/app/app-home/app-home.component.ts
+++ src/app/app-home/app-home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-home.component.css']
 })
 export class AppHomeComponent implements OnInit {
+  showFiller = false;
 
   constructor() { }
 
--- src/app/app-home/app-home.module.ts
+++ src/app/app-home/app-home.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AppHomeRoutingModule} from './app-home-routing.module';
 import {AppHomeComponent} from './app-home.component';
-import {HomeHeaderComponent} from './home-header/home-header.component';
-import {HomeSidebarComponent} from './home-sidebar/home-sidebar.component';
 import {AdministrationModule} from '../administration/administration.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppHomeComponent,
-    HomeHeaderComponent,
-    HomeSidebarComponent],
+    AppHomeComponent],
   imports: [
     CommonModule,
     AppHomeRoutingModule,
-    AdministrationModule
+    AdministrationModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ]
 })
 export class AppHomeModule { }
